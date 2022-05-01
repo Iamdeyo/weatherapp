82c12b7f158b9df53db0839210aefc56dda7b6b9
Deyo <dtechlord@gmail.com>
2022-05-01 16:46:06 +0100

    form init

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(req):
+
+
+    if req.method == "POST":
+        city = req.POST['city']
+        print(city)
+        return redirect('index')
+
+        
     return render(req, 'weather/index.html', {})
